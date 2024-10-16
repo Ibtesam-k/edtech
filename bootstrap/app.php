c651a219f8d4303c58ce9267f3b406aa3148cd79
Ibtesam <ibtesam.kadourah@gmail.com>
2024-10-16 21:48:29 +0300

    Add general exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,9 +1,21 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use App\Http\Middleware\JwtMiddleware;
 use Illuminate\Foundation\Application;
+use Illuminate\Database\QueryException;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +30,51 @@ return Application::configure(basePath: dirname(__DIR__))
         ]); 
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        
+         $exceptions->render(function (MethodNotAllowedHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Method not allowed.'
+                ], 405);
+            }
+        });
+        $exceptions->render(function (NotFoundHttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Record not found.'
+                ], 404);
+            }
+        });
+        $exceptions->render(function (ConnectionException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Connection Error.'
+                ], 503);
+            }
+        });  
+        $exceptions->render(function (QueryException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => 'Database Error.'
+                ], 500);
+            }
+        });  
+        $exceptions->render(function (HttpException $e, Request $request) {
+            if ($request->is('api/*')) {
+                return response()->json([
+                    'message' => $e->getMessage()
+                ], $e->getStatusCode());
+            }
+        });  
+        $exceptions->render(function (Throwable $e, Request $request) {
+            if ($request->is('api/*')) {
+                Log::error($e->getMessage());
+                return response()->json([
+                    'message' => "Someting went wrong!"
+                ],500);
+            }
+        }); 
+
+
     })->create();
